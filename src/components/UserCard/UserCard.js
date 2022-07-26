0107b65220538add7605919be0226ef64d00829e
serhatbek <serhat.bekk@gmail.com>
2022-07-26 21:46:45 +0300

    user card complete

--- src/components/UserCard/UserCard.js
+++ src/components/UserCard/UserCard.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 import { useEffect, useState } from 'react';
+import './UserCard.scss';
 
-const usersData = 'https://randomuser.me/api/?results=10';
+const usersData = 'https://randomuser.me/api/?results=12';
 
 const UserCard = () => {
   const [users, setUsers] = useState([]);
@@ -21,8 +22,17 @@ const UserCard = () => {
   return (
     <>
       {users.map((user) => (
-        <div className='user-card'>
-          <h3>{user.name.first}</h3>
+        <div className='user-card' key={user.dob.date}>
+          <div className='user-card__img flexRowCenter'>
+            <figure>
+              <img src={user.picture.large} alt={user.name.first} />
+            </figure>
+          </div>
+          <div className='user-card__info'>
+            <span>{`${user.name.first} ${user.name.last}`}</span>
+            <span>{user.dob.age}</span>
+            <span>{user.email}</span>
+          </div>
         </div>
       ))}
     </>
--- src/components/pages/Home/Home.js
+++ src/components/pages/Home/Home.js
@@ -1,10 +1,15 @@
 import UserCard from '../../UserCard/UserCard';
+import './Home.scss';
 
 const Home = () => {
   return (
-    <div>
-      Home
-      <UserCard />
+    <div className='home'>
+      <h1>Users</h1>
+      <div className='container'>
+        <div className='box'>
+          <UserCard />
+        </div>
+      </div>
     </div>
   );
 };
