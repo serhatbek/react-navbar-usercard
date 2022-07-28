3aee695de00db09f99aeecb881265cb4f6b4a69f
serhatbek <serhat.bekk@gmail.com>
2022-07-28 21:50:47 +0300

    handleSubmit & handleChange

--- src/components/Form/BasicFormic.js
+++ src/components/Form/BasicFormic.js
@@ -1,4 +1,4 @@
-import { Formik, Field, Form } from 'formik';
+import { Formik } from 'formik';
 
 const BasicFormic = () => {
   return (
@@ -13,29 +13,68 @@ const BasicFormic = () => {
           console.log(values, null, 2);
         }}
       >
-        <Form>
-          <label htmlFor='firstName'>First Name</label>
-          <Field id='firstName' name='firstName' placeholder='Jane' />
-          <br />
-          <br />
-          <label htmlFor='lastName'>Last Name</label>
-          <Field id='lastName' name='lastName' placeholder='Doe' />
-          <br />
-          <br />
-          <label htmlFor='email'>Email</label>
-          <Field
-            id='email'
-            name='email'
-            placeholder='jane@acme.com'
-            type='email'
-          />
-          <br />
-          <br />
-          <button type='submit'>Submit</button>
-        </Form>
+        {({ handleSubmit, handleChange }) => (
+          <form onSubmit={handleSubmit}>
+            <label htmlFor='firstName'>First Name</label>
+            <input
+              id='firstName'
+              name='firstName'
+              placeholder='Jane'
+              onChange={handleChange}
+            />
+            <br />
+            <br />
+            <label htmlFor='lastName'>Last Name</label>
+
+            <input
+              id='lastName'
+              name='lastName'
+              placeholder='Doe'
+              onChange={handleChange}
+            />
+            <br />
+            <br />
+            <label htmlFor='email'>Email</label>
+            <input
+              id='email'
+              name='email'
+              placeholder='jane@acme.com'
+              onChange={handleChange}
+              type='email'
+            />
+            <br />
+            <br />
+            <button type='submit'>Submit</button>
+          </form>
+        )}
       </Formik>
     </div>
   );
 };
 
 export default BasicFormic;
+
+// import { Formik, Field, Form } from 'formik';
+
+{
+  /* <Form>
+<label htmlFor='firstName'>First Name</label>
+<Field id='firstName' name='firstName' placeholder='Jane' />
+<br />
+<br />
+<label htmlFor='lastName'>Last Name</label>
+<Field id='lastName' name='lastName' placeholder='Doe' />
+<br />
+<br />
+<label htmlFor='email'>Email</label>
+<Field
+  id='email'
+  name='email'
+  placeholder='jane@acme.com'
+  type='email'
+/>
+<br />
+<br />
+<button type='submit'>Submit</button>
+</Form> */
+}
